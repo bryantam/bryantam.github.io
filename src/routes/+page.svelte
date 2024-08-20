3c6a4dda2af6cdcd1f8907f15551fa0a8681bdbc
Bryan Tam <tammanho.bryan@gmail.com>
2024-08-19 22:19:25 -0400

    Personalize front page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,11 @@
 <!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+<script>
+    import {fade} from "svelte/transition"
+</script>
 
 <div class="container h-full mx-auto flex justify-center items-center">
 	<div class="space-y-10 text-center flex flex-col items-center">
-		<h2 class="h2">Welcome to Skeleton.</h2>
+		<h2 class="h2">Welcome to my experimental ground for Svelte and Skeleton.</h2>
 		<!-- Animated Logo -->
 		<figure>
 			<section class="img-bg" />
@@ -28,11 +31,13 @@
 				Launch Documentation
 			</a>
 		</div>
+
 		<div class="space-y-2">
 			<p>Try editing the following:</p>
 			<p><code class="code">/src/routes/+layout.svelte</code></p>
 			<p><code class="code">/src/routes/+page.svelte</code></p>
 		</div>
+        <div class="card p-4 variant-filled transitionIn={fade}">Test Card</div>
 	</div>
 </div>
 
